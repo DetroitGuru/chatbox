c44c9bc0a9377a3d721c918a1fc35ce5babe79dd
Detroit Guru <blck4rmy@gmail.com>
2024-11-29 03:38:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template, session, abort
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+load_dotenv()
 from datetime import datetime
 import uuid
 import os
